706dbe5e2d805ab4d03e48533e40d1f62b994dae
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2016-02-20 15:50:45 -0600

    first

---
+++ README.md
@@ -0,0 +1,18 @@
+# problem535 - mcandre's attempt at Project Euler problem 535
+
+https://projecteuler.net/problem=535
+
+Or see index.html if resource is down.
+
+# REQUIREMENTS
+
+* [Haskell](https://www.haskell.org/)
+
+E.g. `brew install ghc`
+
+# EXAMPLE
+
+```
+$ ./problem535.hs
+...
+```
---
+++ problem535.hs
@@ -0,0 +1,25 @@
+#!/usr/bin/env runhaskell
+
+-- fractal series 1, 1, 2, 1, 3, 2, 4, 1, 5, 3, 6, 2, 7, 8, 4, 9, 1, 10, 11, 5, ...
+s :: Integer -> Integer
+s 1 = 1 -- from t
+s n = undefined
+
+-- sum of [s 1, .., s n]
+t :: Integer -> Integer
+t 1 = 1                           -- from spec
+t 20 = 86                         -- from spec
+t 1000 = 364089                   -- from spec
+t 1000000000 = 498676527978348241 -- from spec
+t n = undefined                   -- like (sum $ take n $ map s [1..]), but for integrals
+
+t' :: Integer -> Integer
+t' = ((flip mod) 1000000000) . t  -- from spec
+
+shout :: (Show a) => String -> a -> IO ()
+shout label value = putStrLn (label ++ ":") >> print value
+
+main :: IO ()
+main = do
+  shout "S(1)" $ s 1
+  shout "T'(10^18)" $ t' 10^18
