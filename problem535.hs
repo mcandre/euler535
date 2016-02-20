b58b4cce6199e3abd86b995bbda3a92e8c9ed426
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2016-02-20 17:09:06 -0600

    print s(1:20)_given

--- problem535.hs
+++ problem535.hs
@@ -1,5 +1,7 @@
 #!/usr/bin/env runhaskell
 
+import Control.Exception.Base (assert)
+
 -- fractal series 1, 1, 2, 1, 3, 2, 4, 1, 5, 3, 6, 2, 7, 8, 4, 9, 1, 10, 11, 5, ...
 s :: Integer -> Integer
 s 1 = 1 -- from t
@@ -19,8 +21,16 @@ t' = ((flip mod) 1000000000) . t  -- from spec
 shout :: (Show a) => String -> a -> IO ()
 shout label value = putStrLn (label ++ ":") >> print value
 
+s_given = [1, 1, 2, 1, 3, 2, 4, 1, 5, 3, 6, 2, 7, 8, 4, 9, 1, 10, 11, 5]
+
 main :: IO ()
 main = do
   shout "S(1)" $ s 1
+
+  shout "S(1:20)_given" s_given
+
   shout "S(1:20)" $ map s $ take 20 [1..]
+
+  shout "S(1:20) = S(1:20)_given" $ (map s (take 20 [1..])) == s_given
+
   shout "T'(10^18)" $ t' 10^18
