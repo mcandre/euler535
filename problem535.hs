755442f3bd8f681b5fdad0a73c86904f2cb22978
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2016-02-20 17:14:44 -0600

    break up test cases

--- problem535.hs
+++ problem535.hs
@@ -9,10 +9,6 @@ s n = (n) `mod` (n `div` 2) -- or something
 
 -- sum of [s 1, .., s n]
 t :: Int -> Integer
-t 1 = 1                           -- from spec
-t 20 = 86                         -- from spec
-t 1000 = 364089                   -- from spec
-t 1000000000 = 498676527978348241 -- from spec
 t n = sum $ take n $ map s [1..]
 
 t' :: Int -> Integer
@@ -21,16 +17,28 @@ t' = ((flip mod) 1000000000) . t  -- from spec
 shout :: (Show a) => String -> a -> IO ()
 shout label value = putStrLn (label ++ ":") >> print value
 
-s_given = [1, 1, 2, 1, 3, 2, 4, 1, 5, 3, 6, 2, 7, 8, 4, 9, 1, 10, 11, 5]
+s1_20_given = [1, 1, 2, 1, 3, 2, 4, 1, 5, 3, 6, 2, 7, 8, 4, 9, 1, 10, 11, 5]
+
+t1_given = 1
+t20_given = 86
+t1000_given = 364089
+t1000000000_given = 498676527978348241
 
 main :: IO ()
 main = do
-  shout "S(1)" $ s 1
-
-  shout "S(1:20)_given" s_given
-
+  shout "S(1:20)_given" s1_20_given
   shout "S(1:20)" $ map s $ take 20 [1..]
 
-  shout "S(1:20) = S(1:20)_given" $ (map s (take 20 [1..])) == s_given
+  -- shout "T(1)_given" t1_given
+  -- shout "T(1)" $ t 1
 
-  shout "T'(10^18)" $ t' 10^18
+  -- shout "T(20)_given" t20_given
+  -- shout "T(20)" $ t 20
+
+  -- shout "T(1000)_given" t1000_given
+  -- shout "T(1000" $ t 1000
+
+  -- shout "T(10^9)_given" t1000000000_given
+  -- shout "T(10^9)" $ t (10^9)
+
+  -- shout "T'(10^18)" $ t' 10^18
