57f643cfd21b296bc98e4dfd3d262ca53534f1c9
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2016-02-22 21:24:07 -0600

    open form function working

--- problem535.hs
+++ problem535.hs
@@ -1,39 +1,45 @@
 #!/usr/bin/env runhaskell
 
-import qualified Data.Map as M
+data Mode = Run | Walk
 
-isqrt :: Integer -> Integer
+instance Enum Mode where
+  fromEnum Run = 0
+  fromEnum Walk = 1
+
+  toEnum 0 = Run
+  toEnum 1 = Walk
+
+  succ Run = Walk
+  succ Walk = Run
+
+isqrt :: Integral a => a -> a
 isqrt = floor . sqrt . fromIntegral
 
-isin :: Integer -> Integer
-isin = floor . sin . fromIntegral
-
-ilog :: Integer -> Integer
-ilog = floor . log . fromIntegral
-
 -- fractal series such as S = 1, 1, 2, 1, 3, 2, 4, 1, 5, 3, 6, 2, 7, 8, 4, 9, 1, 10, 11, 5, ...
-s :: Integer -> Integer
-s n = s' (M.empty :: M.Map Integer Integer) 1 n
+s :: [Int]
+s = [1] ++ (s' [2..] 0 Walk)
   where
-    s' :: M.Map Integer Integer -> Integer -> Integer -> Integer
-    s' tracks track 0 = case M.lookup track tracks of
-      Just ai -> ai
-      _ -> -42 -- easier error checking
-    s' tracks track n = s' tracks' track' n'
+    s' :: [Int] -> Int -> Mode -> [Int]
+    s' ns p mode = items ++ (s' ns' p' mode')
       where
-        tracks' = M.insert track h tracks
-        h = 1 + M.findWithDefault 0 track tracks
-        track' = g n tracks'
-        n' = n - 1
+        p' = case mode of
+          Walk -> p
+          Run -> p + 1
 
-        g :: Integer -> M.Map Integer Integer -> Integer
-        g n tracks = (n+1) `mod` (toInteger (M.size tracks))
+        b = s !! p'
 
--- sum of [s 1, .., s n]
-t :: Int -> Integer
-t n = sum $ take n $ map s [1..]
+        (items, ns') = case mode of
+          Walk -> ([b], ns)
+          Run -> splitAt (isqrt b) ns
 
-t' :: Int -> Integer
+        mode' = succ mode
+
+-- sum of [s !! 0, .., s !! (n-1)]
+t :: Int -> Int
+t n = (sum . take n) s
+
+-- t modulo 10^9
+t' :: Int -> Int
 t' = ((flip mod) 1000000000) . t  -- from spec
 
 shout :: (Show a) => String -> a -> IO ()
@@ -49,18 +55,18 @@ t1000000000_given = 498676527978348241
 main :: IO ()
 main = do
   shout "S(1:20)_given" s1_20_given
-  shout "S(1:20)" $ map s $ take 20 [1..]
+  shout "S(1:20)" $ take 20 s
 
-  -- shout "T(1)_given" t1_given
-  -- shout "T(1)" $ t 1
+  shout "T(1)_given" t1_given
+  shout "T(1)" $ t 1
 
-  -- shout "T(20)_given" t20_given
-  -- shout "T(20)" $ t 20
+  shout "T(20)_given" t20_given
+  shout "T(20)" $ t 20
 
-  -- shout "T(1000)_given" t1000_given
-  -- shout "T(1000" $ t 1000
+  shout "T(1000)_given" t1000_given
+  shout "T(1000" $ t 1000
 
-  -- shout "T(10^9)_given" t1000000000_given
-  -- shout "T(10^9)" $ t (10^9)
+  shout "T(10^9)_given" t1000000000_given
+  shout "T(10^9)" $ t (10^9)
 
   -- shout "T'(10^18)" $ t' 10^18
