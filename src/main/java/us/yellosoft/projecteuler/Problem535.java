c545baf6977592440d60e4bb6b4b6b99479055c7
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2016-03-04 23:05:16 -0600

    arrays more performant than collections

--- src/main/java/us/yellosoft/projecteuler/Problem535.java
+++ src/main/java/us/yellosoft/projecteuler/Problem535.java
@@ -1,47 +1,42 @@
 package us.yellosoft.projecteuler;
 
-import it.unimi.dsi.fastutil.BigList;
-import it.unimi.dsi.fastutil.longs.LongBigArrayBigList;
+import it.unimi.dsi.fastutil.longs.LongBigArrays;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
-import java.util.stream.Collectors;
-import java.util.function.BinaryOperator;
-
-public final class Problem535 implements Iterable<Long> {
+public final class Problem535 {
   public static long lsqrt(final long x) {
     return (long) Math.floor(Math.sqrt(x));
   }
 
-  static class S implements Iterator<Long> {
-    private BigList<Long> s;
+  static class S {
+    private long n;
+    private long[][] s;
     private long i;
     private long j;
     private long r;
+    private long h;
+    private long t;
     boolean first;
 
-    public S() {
-      s = new LongBigArrayBigList();
-      i = 2L;
-      j = 0L;
-      r = 0L;
-      first = true;
+    public S(long n) {
+      this.n = n;
+      this.s = LongBigArrays.newBigArray(n);
+      this.i = 2L;
+      this.j = 0L;
+      this.r = 0L;
+      this.h = 0L;
+      this.t = 0L;
+      this.first = true;
     }
 
-    @Override
-    public boolean hasNext() {
-      return true;
+    private void append(long y) {
+      LongBigArrays.set(s, h, y);
     }
 
-    private void append(Long y) {
-      s.add(y);
+    private void add(long y) {
+      t = t + y;
     }
 
-    @Override
-    public Long next() {
+    private void crank() {
       long y;
 
       if (first) {
@@ -51,11 +46,11 @@ public final class Problem535 implements Iterable<Long> {
 
         append(y);
       } else if (r == 0) {
-        y = s.get(j);
+        y = LongBigArrays.get(s, j);
 
         append(y);
 
-        long y2 = s.get(j + 1);
+        long y2 = LongBigArrays.get(s, j + 1);
         j = j + 1;
         r = lsqrt(y2);
       } else {
@@ -67,74 +62,46 @@ public final class Problem535 implements Iterable<Long> {
         r = r - 1;
       }
 
-      return y;
-    }
-  }
-
-  static class Summer implements BinaryOperator<Long> {
-    @Override
-    public Long apply(final Long t, final Long u) {
-      return t + u;
-    }
-  }
-
-  static class ModdingSummer implements BinaryOperator<Long> {
-    private Long modulus;
-
-    public ModdingSummer(final Long modulus) {
-      this.modulus = modulus;
+      add(y);
     }
 
-    @Override
-    public Long apply(final Long t, final Long u) {
-      return (t + u) % modulus;
+    public void generate() {
+      while (h < n) {
+        crank();
+        h++;
+      }
     }
-  }
 
-  @Override
-  public Iterator<Long> iterator() {
-    return new S();
-  }
+    public long sum() {
+      generate();
 
-  public Stream<Long> stream() {
-    return StreamSupport.stream(spliterator(), false);
+      return t;
+    }
   }
 
   public static void main(final String[] args) {
-    final Problem535 problem535 = new Problem535();
-
-    final List<Long> sFirstTwentyGiven = Arrays.asList(new Long[] {
-      1L, 1L, 2L, 1L, 3L, 2L, 4L, 1L, 5L, 3L, 6L, 2L, 7L, 8L, 4L, 9L, 1L, 10L, 11L, 5L
-      });
-    System.out.println("S(1:20)_given:\t" + sFirstTwentyGiven);
-
-    final List<Long> sFirstTwenty = problem535.stream().limit(20).collect(Collectors.toList());
-    System.out.println("S(1:20):\t" + sFirstTwenty);
-
     final Long tFirstGiven = 1L;
     System.out.println("T(1)_given:\t" + tFirstGiven);
 
-    final Summer summer = new Summer();
-
-    final long tFirst = problem535.stream().limit(1).reduce(summer).get();
+    final long tFirst = (new S(1)).sum();
     System.out.println("T(1):\t\t" + tFirst);
 
     final long tTwentyGiven = 86L;
     System.out.println("T(20)_given:\t" + tTwentyGiven);
 
-    final long tTwenty = problem535.stream().limit(20).reduce(summer).get();
+    final long tTwenty = (new S(20)).sum();
     System.out.println("T(20):\t\t" + tTwenty);
 
     final long tOneThousandGiven = 364089L;
     System.out.println("T(1000)_given:\t" + tOneThousandGiven);
 
-    final long tOneThousand = problem535.stream().limit(1000).reduce(summer).get();
+    final long tOneThousand = (new S(1000)).sum();
     System.out.println("T(1000):\t" + tOneThousand);
 
     final long tOneBillionGiven = 498676527978348241L;
     System.out.println("T(10^9)_given:\t" + tOneBillionGiven);
 
-    final long tOneBillion = problem535.stream().limit(1000000000).reduce(summer).get();
+    final long tOneBillion = (new S(1000000000)).sum();
     System.out.println("T(10^9):\t" + tOneBillion);
 
     // final long tOneQuintillion = problem535.stream().limit(1000000000000000000L).reduce(new ModdingSummer(1000000000L)).get();
